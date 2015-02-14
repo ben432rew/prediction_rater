3d7a7cae8a34b36468e2beec77d1b4a87e66d7e9
Benjamin Himley <benjaminhimley85@gmail.com>
2015-02-14 17:00:04 -0500

    adding docstrings

--- controller.py
+++ controller.py
@@ -9,6 +9,7 @@ class Controller(object):
         self.evaluations()
 
     def evaluations(self):
+        '''Display evaluations of data stored in database'''
         results = model.Evaluation.correct_percent(collect_info.websites)
         view.show_evaluations(results)
         non_margs = model.Evaluation.non_marginal_correct(collect_info.websites)
@@ -19,10 +20,12 @@ class Controller(object):
             return True
 
     def todays_predictions(self):
+        '''Display what stocks the websites are predicting today'''
         predicts = model.Database.all_predicts_by_date(datetime.date.today())
         return view.show_todays(predicts)
 
     def consistent_winners(self):
+        ''' Show stocks that are consistently predicted correctly by the websites'''
         winners = model.Evaluation.consistent_winnners()
         view.show_consistent_winners(winners)
 
